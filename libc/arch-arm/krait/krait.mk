719816a7c8aea38336988aaf35ef0be3f215ea65
Brent DeGraaf <bdegraaf@codeaurora.org>
2013-10-03 16:47:18 -0400

    libc: krait: Implement optimized versions of memmove and bcopy
    Restore a jellybean optimization for memmove and bcopy on krait.
    Code has been refactored to thumb2 for consistency with the rest of
    bionic libc, as well as performance and correctness.
    
    Change-Id: I8f2f77dce4534fbce1bdf0188ab353bf34ab8637
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -1,5 +1,6 @@
 libc_bionic_src_files_arm += \
     arch-arm/krait/bionic/memcpy.S \
+    arch-arm/krait/bionic/memmove.S \
     arch-arm/krait/bionic/memset.S \
     arch-arm/krait/bionic/strcmp.S \
     arch-arm/krait/bionic/__strcat_chk.S \
