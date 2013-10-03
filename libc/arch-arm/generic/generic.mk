e5b9afd27255c81176542c706f26972a9ddcfa03
Brent DeGraaf <bdegraaf@codeaurora.org>
2013-10-03 16:37:39 -0400

    libc: arm: Allow optional optimization of memmove and bcopy
    The libc functions memmove and bcopy are not configurable target-by-
    target.  This change allows per-target optimization for best perfor-
    mance.
    
    Change-Id: Id684672fbf0bcffbe68c36a28b64186a6c2554f3
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>

--- libc/arch-arm/generic/generic.mk
+++ libc/arch-arm/generic/generic.mk
@@ -8,3 +8,4 @@ libc_bionic_src_files_arm += \
     bionic/__strcat_chk.cpp \
     bionic/__strcpy_chk.cpp \
     upstream-openbsd/lib/libc/string/strcat.c \
+    upstream-openbsd/lib/libc/string/bcopy.c \
