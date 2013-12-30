00f1e7eeaa157660b5f428b69476e966d60b8aa0
Steve Kondik <shade@chemlab.org>
2013-12-29 18:04:05 -0800

    libc: Enable high-performance memmove and bcopy for cortex-a15
    Change-Id: Id8e4e595021dd13bf4ab5620f5fb0b0d80333616
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>

--- libc/arch-arm/cortex-a15/cortex-a15.mk
+++ libc/arch-arm/cortex-a15/cortex-a15.mk
@@ -1,6 +1,7 @@
 libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/memchr.S \
     arch-arm/cortex-a15/bionic/memcpy.S \
+    arch-arm/krait/bionic/memmove.S \
     arch-arm/cortex-a15/bionic/memset.S \
     arch-arm/cortex-a15/bionic/strcat.S \
     arch-arm/cortex-a15/bionic/strcmp.S \
