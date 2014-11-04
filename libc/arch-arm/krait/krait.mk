54236269a33ccfa88fa7a066789f9396f33e088c
Jake Weinstein <xboxlover360@gmail.com>
2014-11-04 17:06:36 -0600

    libc: remove generic memmove in favor of Krait for A15 and Krait
    Change-Id: Ie2d1c4afb49b521e62e78d5db7c44afb004bc6eb
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -6,10 +6,9 @@ libc_bionic_src_files_arm += \
     arch-arm/krait/bionic/__strcat_chk.S \
     arch-arm/krait/bionic/__strcpy_chk.S \
 
-# Use cortex-a15 versions of strcat/strcpy/strlen/memchr and standard memmove
+# Use cortex-a15 versions of strcat/strcpy/strlen/memchr
 libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/memchr.S \
     arch-arm/cortex-a15/bionic/strcat.S \
     arch-arm/cortex-a15/bionic/strcpy.S \
     arch-arm/cortex-a15/bionic/strlen.S \
-    bionic/memmove.c \
