03787782a3c7acbcf4b71cf9d0ff153c2a8be1e6
Jake Weinstein <xboxlover360@gmail.com>
2015-08-07 14:49:24 +0000

    libm: improve performance by referencing assembly routines
    * From dietlibc
    
    Change-Id: Ie88353b882695e4643af5c8cddf52820e43dbd89

--- libm/upstream-freebsd/lib/msun/src/e_cosh.c
+++ libm/upstream-freebsd/lib/msun/src/e_cosh.c
@@ -40,46 +40,12 @@ __FBSDID("$FreeBSD$");
 #include "math.h"
 #include "math_private.h"
 
-static const double one = 1.0, half=0.5, huge = 1.0e300;
-
 double
 __ieee754_cosh(double x)
 {
-	double t,w;
-	int32_t ix;
-
-    /* High word of |x|. */
-	GET_HIGH_WORD(ix,x);
-	ix &= 0x7fffffff;
-
-    /* x is INF or NaN */
-	if(ix>=0x7ff00000) return x*x;	
-
-    /* |x| in [0,0.5*ln2], return 1+expm1(|x|)^2/(2*exp(|x|)) */
-	if(ix<0x3fd62e43) {
-	    t = expm1(fabs(x));
-	    w = one+t;
-	    if (ix<0x3c800000) return w;	/* cosh(tiny) = 1 */
-	    return one+(t*t)/(w+w);
-	}
-
-    /* |x| in [0.5*ln2,22], return (exp(|x|)+1/exp(|x|)/2; */
-	if (ix < 0x40360000) {
-		t = __ieee754_exp(fabs(x));
-		return half*t+half/t;
-	}
-
-    /* |x| in [22, log(maxdouble)] return half*exp(|x|) */
-	if (ix < 0x40862E42)  return half*__ieee754_exp(fabs(x));
-
-    /* |x| in [log(maxdouble), overflowthresold] */
-	if (ix<=0x408633CE)
-	    return __ldexp_exp(fabs(x), -1);
-
-    /* |x| > overflowthresold, cosh(x) overflow */
-	return huge*huge;
+    long double  y = exp (x);
+    return (y + 1./y) * 0.5;
 }
-
 #if (LDBL_MANT_DIG == 53)
 __weak_reference(cosh, coshl);
 #endif
